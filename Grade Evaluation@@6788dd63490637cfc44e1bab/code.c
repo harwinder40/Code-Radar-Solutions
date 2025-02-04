ade2a66acc025e9fcf85cb380fcd27aa52b4c990
harwinder40 <harwinder1340.becse24@chitkara.edu.in>
2025-02-04 20:35:03 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char word;
+    if (word==A)
+        printf("Excellent");
+    else if(word==B)
+        printf("Good");
+    else if(word==C)
+        printf("Average");
+    else if(word==D)
+        printf("Below Average");
+    else if(word==F)
+        printf("Fail");
+    else
+        printf("Invalid grade");
+    return 0;
+}
